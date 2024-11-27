b498d83a9d3b17ce9ceea8445d82f1e937182dfe
Jerry Li <thermitex@outlook.com>
2024-11-27 21:08:30 +0800

    Correct typo in constants

--- lib/src/common/services/constants.dart
+++ lib/src/common/services/constants.dart
@@ -178,8 +178,8 @@ class Constants {
   static const kAboutPrivacyUrl = 'https://cuckoo-hku.xyz/privacy';
   static const kAboutSoftwareLicense = 'Software Licenses';
   static const kAboutSoftwareLicenseUrl = 'https://cuckoo-hku.xyz/license';
-  static const kCopyDiagnostics = 'Copy Diagnositics';
-  static const kDiagnosticsCopiedPrompt = 'Diagnositic Info Copied';
+  static const kCopyDiagnostics = 'Copy Diagnostics';
+  static const kDiagnosticsCopiedPrompt = 'Diagnostic Info Copied';
   static const kDiagnosticWarning =
       'Diagnostic information contains your private information and should only be shared with someone you trust.';
   static const kProjectGithubUrl =
